9a21f68ba710ff0bf2a2dc13e661c993dce77173
DomenicoSacco94 <domenico@element.in>
2025-03-24 17:13:57 +0100

    reuse the template

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: ".",
+            includes: "_includes",
             output: "_site"
         },
         pathPrefix: "/blog/"
