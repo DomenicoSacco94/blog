ee17c64721f1c9ba90b03184610eefbffa198de6
DomenicoSacco94 <domenico@element.in>
2025-03-24 16:51:58 +0100

    configure eleventy for deployment

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
     // Copy `css` directory to the output
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("contacts");
+    eleventyConfig.addPassthroughCopy("cv");
+    eleventyConfig.addPassthroughCopy("posts");
+    eleventyConfig.addPassthroughCopy("projects");
 
     return {
         dir: {
